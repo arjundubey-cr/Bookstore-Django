f19cf57f0d1b507a2e31279d52f5fc3ece5a3bcb
Arjun Dubey <arjundubeycr@gmail.com>
2022-09-03 13:59:39 +0530

    added test for custom user and admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
+# Here we register the CustomUser form with admin
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+CustomUser = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "is_superuser",
+    ]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="will", email="will@email.com", password="testpass@123"
+        )
+        self.assertEqual(user.username, "will")
+        self.assertEqual(user.email, "will@email.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username="superuser", email="superuser@email.com", password="super@123"
+        )
+        self.assertEqual(admin_user.username, "superuser")
+        self.assertEqual(admin_user.email, "superuser@email.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
